8cfb6cc0b021daed2b15c75faefb67143ef2a284
karolgacon <karolgacon22@gmail.com>
2024-10-08 14:36:38 +0200

    feat: added main file and templates

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app= Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/exchange', methods=["POST"])
+def exchange():
+    # from_currency_code = request.form['from_currency']
+    # to_currency_code = request.form['to_currency']
+    # amount = float(request.form['amount'])
+
+    return render_template('result.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
