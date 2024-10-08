185f7a97dfda85c44c2425c5e1b273fd8ca71988
karolgacon <karolgacon22@gmail.com>
2024-10-08 14:57:27 +0200

    fix: repaired constructor

--- Currency/Currency.py
+++ Currency/Currency.py
@@ -1,10 +1,19 @@
 from ICurrency import ICurrency
 
 class Currency(ICurrency):
-    def __init__(self, name:str, code:str, rate:float):
-        self.name = name
-        self.code = code
-        self.rate = rate
+    def __init__(self,  code:str, name:str = None, rate:float = None):
+        if name is None and rate is None:
+            self.code = code
+        else:
+            self.name = name
+            self.code = code
+            self.rate = rate
+
+    def __eq__(self, __value: object) -> bool:
+        if self.code == __value.getCode():
+            return True
+        else:
+            return False
 
     def setName(self, name: str) -> None:
         self.name = name
